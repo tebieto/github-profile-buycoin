21d97695e77f2b1799f5498d2cb4366119d30f6b
Terry Ebieto <tebieto@gmail.com>
2020-11-14 09:47:10 +0100

    profile UI done

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import { getPageHeader } from './components/header/index.js';
-import { fetchProfileAndRepos} from './utils/index.js';
+// import { getPageHeader } from './components/header/index.js';
+// import { fetchProfileAndRepos} from './utils/index.js';
 
-fetchProfileAndRepos().then(data => console.log({data}));
+// fetchProfileAndRepos().then(data => console.log({data}));
 
-const renderPage = () => {
-    const node = document.getElementById("app");
-    const pageHeader = getPageHeader();
-    if(!node || !pageHeader) return alert('error');
-    const page = pageHeader;
-    node.innerHTML = page;
-}
+// const renderPage = () => {
+//     const node = document.getElementById("app");
+//     const pageHeader = getPageHeader();
+//     if(!node || !pageHeader) return alert('error');
+//     const page = pageHeader;
+//     node.innerHTML = page;
+// }
 
-renderPage();
+// renderPage();
